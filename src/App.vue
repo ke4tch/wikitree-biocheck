63126561f384aa22d9edd6fbb26be3938c95124f
ke4tch <ke4tch@gmail.com>
2024-09-25 10:47:07 -0400

    Check for isApp

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         </a>
     </div>
     <div class="flex-center">
-      <h4>Bio Check Version 1.7.14 TEST</h4>
+      <h4>Bio Check Version 1.7.14</h4>
     </div>
 
     <div class="flex-grid">
